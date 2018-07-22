c5ad7f7b4d8aa8cb60799792bb229f60b3d59158
we1103617438 <we1103617438@qq.com>
2018-07-22 12:33:11 +0800

    00

---
+++ springboot/springboot-hello/src/test/java/com/tanhuan/www/springboothello/SpringbootHelloApplicationTests.java
@@ -0,0 +1,18 @@
+package com.tanhuan.www.springboothello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootHelloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		int a= 1/0;
+		System.out.println(a);
+	}
+
+}
