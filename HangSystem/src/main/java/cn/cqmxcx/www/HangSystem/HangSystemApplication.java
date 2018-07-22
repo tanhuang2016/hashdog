b5bfb77e0cb2cb0768216445b05cea565b46625f
we1103617438 <we1103617438@qq.com>
2018-07-22 14:14:23 +0800

    springboot开头

---
+++ HangSystem/src/main/java/cn/cqmxcx/www/HangSystem/HangSystemApplication.java
@@ -0,0 +1,12 @@
+package cn.cqmxcx.www.HangSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangSystemApplication.class, args);
+	}
+}
